81f8af511c5de5928d007e24d00c09e12c1b4f3c
sonam0296 <sonam43jha@gmail.com>
2024-07-01 22:00:16 +0530

    brute force approach - Palindrome

--- mathematics/palindrome.ts
+++ mathematics/palindrome.ts
@@ -19,3 +19,42 @@ function palindromeNum(value: number | string) : boolean {
 }
 
 console.log(palindromeNum("tatw"))
+
+/**
+ * 
+ * Time Complexity -> O(n)
+ * Space Complexity -> O(1)
+ * 
+ */
+
+/**
+ *  We can solve the above problem with different ways 
+ * 
+ * The above way was using Two Pointer approach 
+ * 
+ * Below is brute force approach
+ * 
+ */
+
+function bruteForcePalindrome(num: number) {
+    // Reverse the num and get the last digit and add to the reverse and compare it
+    let reverse = 0
+    let temp = num
+    while(temp > 0){
+        // Find the last digit
+        let digit = temp % 10
+        // Add the digit to reverse 
+        reverse = reverse * 10 + digit
+        // Remove the last digit from temp
+        temp = Math.floor(temp/10)
+    }
+    
+    if (num === reverse) {
+        return "Palindrome"
+    }
+    else{
+        return 'Not Palindrome'
+    }
+}
+
+console.log(bruteForcePalindrome(8778))
