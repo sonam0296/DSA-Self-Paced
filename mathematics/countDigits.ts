06ee1781ea93f2e3aa888cb3adc27dd58abd5ee8
sonam0296 <sonam43jha@gmail.com>
2024-07-01 21:34:31 +0530

    First Commit

---
+++ mathematics/countDigits.ts
@@ -0,0 +1,18 @@
+function countDigits(num) {
+    // Initializing count i.e 0
+    let count = 0;
+    // Loop till num > 0
+    while(num > 0) {
+        // Start removing the digits from end
+        num = Math.floor(num / 10)
+        count++
+    }
+    return count
+}
+
+console.log(countDigits(768))
+
+/**
+ * Time Complexity -- O(N) ===>> As loop is running n times 
+ * Space Complexity --- O(1)
+ */
